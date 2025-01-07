3f1986c0eafc4c762c4bb688ebd8308014b358e3
Chris-C1008 <chris.cooper1008200@gmail.com>
2025-01-07 20:32:24 +0000

    Delete fairfind-extension: directory

--- fairfind-extension:/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("FairFind Extension Installed! Ready to save ethically.");
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "checkAffiliate") {
-    sendResponse({ status: "Affiliate link checked." });
-  }
-});
